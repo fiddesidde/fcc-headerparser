4316e6c6b67cb4b01ac49d7969b7d2c95239262f
fiddesidde <fredrik.mellberg@gmail.com>
2021-01-13 11:25:13 +0100

    2nd test

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/api/hello', function (req, res) {
 
 app.get('/api/whoami', (req, res) => {
     const obj = {
-        ipaddress: req.ip,
+        ipaddress: req.connection.remoteAddress,
         language: req.headers['accept-language'],
         software: req.headers['user-agent'],
     };
